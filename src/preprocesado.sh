1d62604ee40ee13541a0aebec1bbd032f7b0bd97
Dante Culaciati <programmingdante@gmail.com>
2024-05-26 22:41:00 -0300

    Agrego dataset 2 a script de pre procesado

--- src/preprocesado.sh
+++ src/preprocesado.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 INPUT_DATASET_URL="https://github.com/OpenNeuroDatasets/ds002785"
+INPUT_DATASET2_URL="https://github.com/OpenNeuroDatasets/ds002790"
 INPUT_FOLDER="input_data"
 OUTPUT_FOLDER="output_data"
 DATASET_NAME="brain_hemispheres_dataset"
 DERIVATIVES_PATH="derivatives/vbm"
 
-datalad create $DATASET_NAME
-cd $DATASET_NAME
+DATASETS_URLS=($INPUT_DATASET_URL $INPUT_DATASET2_URL)
 
-datalad install -d . -s $INPUT_DATASET_URL $INPUT_FOLDER
-datalad get $INPUT_FOLDER/$DERIVATIVES_PATH
+for i in "${!DATASET_URLS[@]}"; do
+    datalad create ${DATASET_NAME}$i
+    cd $DATASET_NAME
 
-mkdir -p $OUTPUT_FOLDER
+    datalad install -d . -s ${DATASET_URLS[i]} $INPUT_FOLDER
+    datalad get $INPUT_FOLDER/$DERIVATIVES_PATH
 
-echo "Running python script..."
-python ../process_brain_images.py $INPUT_FOLDER/$DERIVATIVES_PATH $OUTPUT_FOLDER
+    mkdir -p $OUTPUT_FOLDER
 
-datalad save -m "Processed brain images: split hemispheres and flipped left hemisphere" $OUTPUT_FOLDER
-echo "Processing complete. Dataset is ready."
+    echo "Running python script..."
+    python ../process_brain_images.py $INPUT_FOLDER/$DERIVATIVES_PATH $OUTPUT_FOLDER
+
+    datalad save -m "Processed brain images: split hemispheres and flipped left hemisphere" $OUTPUT_FOLDER
+
+    echo "Processing complete. Dataset $i is ready."
+    
+    cd ..
+done
